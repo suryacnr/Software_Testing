7242461f77718e8d6b2173f43b98b82e05c92349
suryacnr <45647852+suryacnr@users.noreply.github.com>
2022-11-06 14:27:15 +0530

    Update assertions.java

--- TesntNg/assertions.java
+++ TesntNg/assertions.java
@@ -6,9 +6,13 @@ import org.testng.asserts.SoftAssert;
 
 public class Assert_softAssert {
 	//hard assert
+	// hard assert means if the assertion fails all the crosponding lines will not execute
 	@Test
 	public void assertion()
 	{
+		Syso("open browser");
+		Assert.assertEquals(true,faile); //if this line failes the below line will  not execute this is called hard assert
+		Syso("enter the url");
 		Assert.assertEquals(12, 13 ,"Drop down does not match please check");
 		
 	}
@@ -19,14 +23,27 @@ public class Assert_softAssert {
 		Assert.assertTrue(condtion.contains("surya"), "names are not matching");
 	}
 	//soft assert
+	//if we are doing some validation in the test cases and after that also there some line of code to execute then you can put soft assert
+	//sofft assert will not terminate the program
+	//if you put soft assert and if test case fails it will not show in the report as fail to solve problem we have to use Softassert.asserAll(); at the end of the test ccases
 	@Test
 	public void softassert()
 	{
+		Syso("open browser");
+		SoftAssert.assertEquals(true,faile); //if this line failes the below line will  not execute this is called hard assert
+		Syso("enter the url");
 		SoftAssert assertion = new SoftAssert();
-		assertion.assertEquals(12, 13);
-		assertion.assertAll();
+		assertion.assertEquals(12, 13  "Home page is missing");
+		assertion.assertAll();//if test case fails it will not show in the report as fail to solve problem we have to use Softassert.asserAll(); at the end of the test ccases
 		
 	}
+	
+	//Dont put inside this method if you put it fail only teardownclass so you have to use befor each class
+	@AfterClass
+	public void tearndownclass(){
+		softeassert.assertALL();
+	}
+	
 	
 
 }
